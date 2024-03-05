605dc1e834aaef5aaab8850167f815a75835d842
Duncan Doyle <Duncan.Doyle@gmail.com>
2024-03-05 13:58:17 +0100

    Fixed some print statements in setup script.

--- install/setup.sh
+++ install/setup.sh
@@ -6,7 +6,7 @@ printf "\nInstall Keycloak ...\n"
 # Create Keycloak namespace if it does not yet exist
 kubectl create namespace keycloak --dry-run=client -o yaml | kubectl apply -f -
 
-kubectl apply -f keycloak/secrets.yaml
+kubectl apply -f keycloak/keycloak-secrets.yaml
 kubectl apply -f keycloak/keycloak-db-pv.yaml
 kubectl apply -f keycloak/keycloak-postgres.yaml
 printf "\nWait for Keycloak Postgres readiness ...\n"
